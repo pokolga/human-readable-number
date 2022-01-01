8bdd6a2572dd00cdcd7d1fb2a3d6ab7bb819b937
pokolga <pokolga1801@gmail.com>
2022-01-01 21:53:16 +0300

    init: task reverse digits of the input number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    if (number === 0) return "zero";
+    const units = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const dozens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const hundreds = parseInt(number / 100);
+    const rankTeens = number % 100;
+    const rankUnits = number % 10;
+    if (rankTeens <= 19 && rankTeens >= 10) {
+        let result = `${units[hundreds]}${!(hundreds) ? "" : " hundred"} ${teens[rankUnits]}`
+        return result.replace(/\s+/g, " ").trim();
+        //return units[hundreds] + (!hundreds) ? "" : " hundred" + teens[rankTeens];
+    }
+
+    const rankDozens = (number % 100 - rankUnits) / 10;
+    let result = `${units[hundreds]}${!(hundreds) ? "" : " hundred"} ${dozens[rankDozens]} ${units[rankUnits]}`;
+    return result.replace(/\s+/g, " ").trim();
 }
